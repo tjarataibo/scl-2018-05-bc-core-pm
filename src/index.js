708e4bb0936a520eb7d59dbfcdedcbc5f2966969
Tamara <tjarataibo@gmail.com>
2018-05-24 22:09:06 -0400

    Modifiqué la fachada del index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-/* Acá va tu código */
+Var processNumber = function ()
+
+{
+    var numberOne=  document.getElementById("lnumber").value;
+    document.getElementById('box-number').style.display = 'none';
+
+}
