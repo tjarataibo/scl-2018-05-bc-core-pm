0afdf2a8c16a9d112fd421f7650243a7618b0d85
Tamara <tjarataibo@gmail.com>
2018-05-31 15:02:31 -0400

    Crear función de los valores respectivos de cifrado y descifrado para que aparezca en HTML

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-Var processNumber = function ()
 
+//Crear función de los valores respectivos de cifrado para que aparezca en HTML (obtener datos de cifrado)
+
+encodeGetData = () => 
 {
-    var numberOne=  document.getElementById("lnumber").value;
-    document.getElementById('box-number').style.display = 'none';
+    let offset= parseInt(document.getElementById("offset").value);
+    let string=  document.getElementById("text").value;
+    document.getElementById("message").innerHTML = window.cipher.encode(offset, string);
+}
 
+//Crear función de los valores respectivos de descifrado para que aparezca en HTML (obtener datos de descifrado)
+
+decodeGetData = () => 
+{
+    let offset= parseInt(document.getElementById("offset").value);
+    let string= document.getElementById("text").value; 
+    document.getElementById("message").innerHTML = window.cipher.decode(offset, string);
 }
